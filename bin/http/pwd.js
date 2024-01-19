e53dec397a37267c4dc0e97ef4714627caa945fb
李子昊子李 <97011124+iLeepop@users.noreply.github.com>
2024-01-19 18:25:02 +0800

    feat: simple live-server (#2)
    * feat: live-server build
    
    * feat: finish live-server

---
+++ bin/http/pwd.js
@@ -0,0 +1,6 @@
+import { fileURLToPath } from "node:url"
+import path from "node:path"
+
+export const __filename = fileURLToPath(import.meta.url)
+export const __dirname = path.dirname(__filename)
+export const pwd = path.resolve(__dirname, process.cwd())
--- bin/http/server.js
+++ bin/http/server.js
@@ -1,17 +1,38 @@
 import express from 'express'
 import chalk from 'chalk'
-import { rootStatic, rootIndex } from './static.js'
+import bodyParser from 'body-parser'
+import http from 'node:http'
+import { rootStatic, rootIndex, staticHandler } from './static.js'
+import { createWebSocket } from './ws.js'
 
 const app = express()
 
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
 export const serstart = (port) => {
 
-  if (true) {
+  if (false) {
     console.log(chalk.bold.yellow('Enable Static Access'))
-    app.use(rootStatic).use(rootIndex)
+    app.use(rootStatic)
   }
 
-  app.listen(port, () => {
+  if (true) {
+    console.log(chalk.bold.yellow('Enable Static Handler Access'))
+    app.use(staticHandler)
+  }
+
+  if (true) {
+    console.log(chalk.bold.yellow('Enable Index Access'))
+    app.use(rootIndex)
+  }
+
+  const server = http.createServer(app)
+
+  server.addListener('upgrade', createWebSocket)
+
+  server.listen(port, () => {
     console.log(chalk.bold.green(`Server on http://localhost:${port}`))
   })
+
+  return server
 }
